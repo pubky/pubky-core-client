eb148c9042fee322db6d33323abf2ce25081eab7
dzdidi <deniszalessky@gmail.com>
2024-03-26 13:47:11 +0100

    pkarr: initial draft lookup
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+mod transport;
 mod transport_fs;
 mod paykit;
 
-fn main() { }
+fn main() { 
+    transport::pkarr_lookup("hexxxkc4rn3c6hsg17eugwbz88ci9qsa4f87qe85e89jstmmfo5o".try_into().expect("failed key"));
+}
