b4c02520d52cbb0f68bcf99f9dda1eec74900b3f
dzdidi <deniszalessky@gmail.com>
2024-03-28 17:31:17 +0100

    pkarr: url extraction (broken)
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod transport;
-use crate::transport::pkarr::lookup;
+use crate::transport::pkarr::{lookup, resolve_homeserver_url};
 use pkarr::DEFAULT_PKARR_RELAY;
 
 use reqwest::Url;
@@ -9,9 +9,10 @@ mod paykit;
 
 
 fn main() { 
-    let res = lookup("hexxxkc4rn3c6hsg17eugwbz88ci9qsa4f87qe85e89jstmmfo5o".try_into().expect("failed key"), "_foo", Option::<&Url>::None);
+    let res = resolve_homeserver_url("hexxxkc4rn3c6hsg17eugwbz88ci9qsa4f87qe85e89jstmmfo5o".try_into().expect("failed key"), Option::<&Url>::None);
     println!("DHT: {:?}", res);
 
-    let res = lookup("hexxxkc4rn3c6hsg17eugwbz88ci9qsa4f87qe85e89jstmmfo5o".try_into().expect("failed key"), "_foo", Some(&Url::parse(DEFAULT_PKARR_RELAY).unwrap()));
+    let relay_url = Url::parse(DEFAULT_PKARR_RELAY).unwrap();
+    let res = resolve_homeserver_url("hexxxkc4rn3c6hsg17eugwbz88ci9qsa4f87qe85e89jstmmfo5o".try_into().expect("failed key"), Some(&relay_url));
     println!("Relay: {:?}", res);
 }
--- src/transport/pkarr.rs
+++ src/transport/pkarr.rs
@@ -1,30 +1,53 @@
 use reqwest::Url;
-use pkarr::{PkarrClient, PublicKey, dns };
+use pkarr::{PkarrClient, PublicKey, dns, SignedPacket};
 
-use crate::transport::pkarr::dns::rdata::RData::TXT;
-
-// Look up:
-// take public key as an input and resolve dns record to the data store
-//
-pub fn lookup(public_key: PublicKey, name: &str, relay_url: Option<&Url>) -> Result<String, String> {
+pub fn lookup<'a>(public_key: PublicKey, relay_url: Option<&'a Url>) -> Result<SignedPacket, String> {
     let client = PkarrClient::new();
     let entry = match relay_url {
         Some(relay_url) => client.relay_get(relay_url, public_key).unwrap(),
         None => client.resolve_most_recent(public_key)
     };
-    let mut res = String::new();
+
     match entry {
         None => return Err("No entry found".to_string()),
-        Some(entry) => entry.resource_records(name).for_each(|record| {
-            res = if let TXT(txt) = &record.rdata { txt.clone().try_into().unwrap() } else { "".to_string() };
-        })
+        Some(entry) => Ok(entry)
+    }
+}
+// /// Resolves home server url using relay (with name '_pubky')
+// pub fn resolve_homeserver(public_key: &str, relay_url: Option<&Url>) -> Result<&str, Strign> {
+//     let key = match lookup(public_key, "_pubky", relay_url) {
+//         Err(e) => return Err(e),
+//         Ok(key) => key
+//     };
+//
+//     match resolve_homeserver_url(key) {
+//         Err(e) => return Err(e),
+//         Ok(url) => {
+//             // set to cache
+//             Ok(url)
+//         }
+//     }
+// }
+//
+/// Resolves home server url using relay (with name '@')
+pub fn resolve_homeserver_url(public_key: PublicKey, relay_url: Option<&Url>) -> Result<Url, String> {
+    let packet = match lookup(public_key, relay_url) {
+        Err(e) => return Err(e),
+        Ok(key) => key
+    };
+
+    let records = packet.resource_records("_foo");
+
+    for record in records {
+        match &record.rdata {
+            // FIXME: formatting/concatenation is broken here.
+            // corresponding ResouceRecords have method `to_string` but their trait bound are not satisfied
+            dns::rdata::RData::CNAME(cname) => return Ok(Url::parse(format!("https://{:?}", cname).as_str()).unwrap()),
+            dns::rdata::RData::TXT(txt) => return Ok(Url::parse(format!("http://{:?}", txt).as_str()).unwrap()),
+            _ => continue,
+        }
     }
 
-    if res.is_empty() { Err("No TXT record found".to_string()) } else { Ok(res) }
+    return Err("No records found".to_string());
 }
-
-// Resolves home server url using relay (with name '_pubky')
-// pub fn resolve_homeserver(&self, public_key: &str) -> Result<&str, Error> {}
-
-// Resolves home server url using relay (with name '@')
-// pub fn resolve_homeserver_url(&self, public_key: &str) -> Result<&str, Error> {}
+//
