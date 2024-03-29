e747d1500f10eefc7b3b51b7937e51af07980135
dzdidi <deniszalessky@gmail.com>
2024-03-29 15:01:21 +0100

    pkarr: resolver class; caching 1 strat
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 mod transport;
-use crate::transport::pkarr::{lookup, resolve_homeserver_url};
+use crate::transport::resolver::Resolver;
 use pkarr::DEFAULT_PKARR_RELAY;
 
 use reqwest::Url;
 
-mod transport_fs;
 mod paykit;
+mod transport_fs;
 
+fn main() {
+    let key = "hexxxkc4rn3c6hsg17eugwbz88ci9qsa4f87qe85e89jstmmfo5o";
+    let relay_url = Url::parse(&DEFAULT_PKARR_RELAY).unwrap();
 
-fn main() { 
-    let res = resolve_homeserver_url("hexxxkc4rn3c6hsg17eugwbz88ci9qsa4f87qe85e89jstmmfo5o".try_into().expect("failed key"), Option::<&Url>::None);
+    let mut resolver = Resolver::new(Option::<&Url>::None);
+    let res =
+        resolver.resolve_homeserver(key.try_into().expect("failed key"), Option::<&Url>::None);
     println!("DHT: {:?}", res);
 
-    let relay_url = Url::parse(DEFAULT_PKARR_RELAY).unwrap();
-    let res = resolve_homeserver_url("hexxxkc4rn3c6hsg17eugwbz88ci9qsa4f87qe85e89jstmmfo5o".try_into().expect("failed key"), Some(&relay_url));
-    println!("Relay: {:?}", res);
+    let res = resolver.resolve_homeserver(key.try_into().expect("failed key"), Some(&relay_url));
+    println!("DHT (relay): {:?}", res);
+
+    let mut resolver = Resolver::new(Some(&relay_url));
+    let res =
+        resolver.resolve_homeserver(key.try_into().expect("failed key"), Option::<&Url>::None);
+    println!("Resolver Relay: {:?}", res);
+
+    let res = resolver.resolve_homeserver(key.try_into().expect("failed key"), Some(&relay_url));
+    println!("Method Relay: {:?}", res);
 }
---
+++ src/transport/resolver.rs
@@ -0,0 +1,140 @@
+use pkarr::{dns, PkarrClient, PublicKey, SignedPacket};
+use reqwest::Url;
+use std::collections::HashMap;
+
+pub struct Resolver<'a> {
+    relay_url: Option<&'a Url>,
+    // TODO: add suport for different cache strategeies:
+    // - read through
+    // - read around
+    // - read ahead
+    // - read behind (current implementation)
+
+    cache: HashMap<String, Url>,
+}
+
+impl Resolver<'_> {
+    pub fn new(relay_url: Option<&Url>) -> Resolver {
+        Resolver {
+            relay_url,
+            cache: HashMap::new(),
+        }
+    }
+    /// Resolves home server url using relay (with name '_pubky')
+    // TODO: add option to skip cache
+    pub fn resolve_homeserver(
+        &mut self,
+        public_key: PublicKey,
+        relay_url: Option<&Url>,
+    ) -> Result<&Url, String> {
+        if self.cache.contains_key(&public_key.to_string()) {
+            return Ok(self.cache.get(&public_key.to_string()).expect(
+                "Failed to get value from cache"
+            ));
+        }
+
+        let packet = match self.lookup(&public_key, relay_url) {
+            Err(e) => return Err(e),
+            Ok(key) => key,
+        };
+
+        // TODO: move into  lookup
+        let records = packet.resource_records("_pubky");
+
+        for record in records {
+            match &record.rdata {
+                dns::rdata::RData::TXT(txt) => {
+                    // See https://docs.rs/simple-dns/latest/simple_dns/rdata/struct.TXT.html#method.attributes
+                    for (k, v) in txt.attributes() {
+                        if !k.starts_with("home") {
+                            continue;
+                        }
+
+                        match v {
+                            None => return Err("No value found".to_string()),
+                            Some(v) => match self.resolve_homeserver_url(
+                                v.as_str().try_into().expect("failed key"),
+                                relay_url,
+                            ) {
+                                Err(e) => return Err(e),
+                                Ok(url) => {
+                                    &self.cache.insert(public_key.to_string(), url.clone());
+
+                                    return Ok(self.cache.get(&public_key.to_string()).expect(
+                                        "Failed to get value from cache"
+                                    ));
+                                }
+                            },
+                        }
+                    }
+                }
+                _ => continue,
+            }
+        }
+
+        Err("No records found".to_string())
+    }
+
+    /// Resolves home server url using relay (with name '@')
+    fn resolve_homeserver_url(
+        &self,
+        public_key: PublicKey,
+        relay_url: Option<&Url>,
+    ) -> Result<Url, String> {
+        let packet = match self.lookup(&public_key, relay_url) {
+            Err(e) => return Err(e),
+            Ok(key) => key,
+        };
+
+        let records = packet.resource_records("@");
+
+        for record in records {
+            match &record.rdata {
+                dns::rdata::RData::CNAME(cname) => {
+                    // See https://docs.rs/simple-dns/latest/simple_dns/rdata/struct.CNAME.html#fields
+                    return Ok(
+                        Url::parse(format!("https://{}", cname.0.to_string()).as_str()).unwrap(),
+                    );
+                }
+                dns::rdata::RData::TXT(txt) => {
+                    // See https://docs.rs/simple-dns/latest/simple_dns/rdata/struct.TXT.html#method.attributes
+                    for (k, v) in txt.attributes() {
+                        if !k.starts_with("localhost") {
+                            continue;
+                        }
+                        match v {
+                            Some(v) => {
+                                return Ok(Url::parse(format!("http://{k}{v}").as_str()).unwrap())
+                            }
+                            None => return Ok(Url::parse(format!("http://{k}").as_str()).unwrap()),
+                        }
+                    }
+                }
+                _ => continue,
+            }
+        }
+
+        Err("No records found".to_string())
+    }
+
+    fn lookup<'a>(
+        &self,
+        public_key: &PublicKey,
+        relay_url: Option<&Url>,
+    ) -> Result<SignedPacket, String> {
+        let client = PkarrClient::new();
+        let public_key = public_key.clone();
+        let entry = match relay_url {
+            Some(relay_url) => client.relay_get(relay_url, public_key).unwrap(),
+            None => match &self.relay_url {
+                Some(relay_url) => client.relay_get(&relay_url, public_key).unwrap(),
+                None => client.resolve_most_recent(public_key),
+            },
+        };
+
+        match entry {
+            None => return Err("No entry found".to_string()),
+            Some(entry) => Ok(entry),
+        }
+    }
+}
