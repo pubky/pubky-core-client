3d786dcb04491cd208de679d95e5c03bd184b16b
dzdidi <deniszalessky@gmail.com>
2024-04-02 10:05:30 +0200

    crypto: add random_bytes generator
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/crypto.rs
+++ src/crypto.rs
@@ -2,6 +2,13 @@
 #[allow(unused_imports)]
 pub use blake3;
 
+use rand::rngs::OsRng;
+use rand::RngCore;
+
+// Re-exporting public key for the challenge
+#[allow(unused_imports)]
+pub use pkarr::PublicKey;
+
 use ed25519_dalek::SigningKey;
 
 use pkarr::Keypair;
@@ -26,6 +33,14 @@ pub fn zeroize(buf: &mut [u8]) {
     for byte in buf.iter_mut() {
         *byte = 0;
     }
+}
+
+pub fn random_bytes(len: usize) -> Vec<u8> {
+    let mut rng = OsRng;
+    let mut buffer = vec![0u8; len];
+    rng.fill_bytes(&mut buffer);
+
+    buffer
 }
 
 #[cfg(test)]
@@ -53,5 +68,16 @@ mod tests {
             Keypair::generate(Some(seed_1)).to_z32(),
             Keypair::generate(Some(seed_2)).to_z32()
         );
+    }
+
+    #[test]
+    fn test_random_bytes() {
+        let len = 32;
+        let bytes_1 = random_bytes(len);
+        let bytes_2 = random_bytes(len);
+
+        assert_eq!(bytes_1.len(), len);
+        assert_eq!(bytes_2.len(), len);
+        assert_ne!(bytes_1, bytes_2);
     }
 }
