b31214283e693be8ef3fd51e59b18546e0f1c06a
dzdidi <deniszalessky@gmail.com>
2024-04-09 18:30:20 +0200

    auth: signup draft
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/transport/auth.rs
+++ src/transport/auth.rs
@@ -26,24 +26,46 @@ impl Auth {
     }
 
     /// Create a new account at the config homeserver
-    pub fn signup(seed: &str) -> Result<&str, Error> {
-        // TODO:
-        // generate keypair from seed
-        let keypair = DeterministicKeyGen::generate(Some(seed));
-        // send user root signature as signup
-        // create signed packet with keypair and homeserverId
-        // userId = encode public key into z32
-        // PUT signed packet to homeserver /mvp/users/{userId}/pkarr as `application/octet-stream`
-        // cache homeserver_url accessible via userId
-        // zeroize private keypair
-        // return userId
+    pub fn signup(seed: &str, relay_url: Option<&Url>) -> Result<&str, String> {
+        let key_pair = DeterministicKeyGen::generate(Some(seed));
+        let userId = match self.send_user_root_signature(SigType::Signup, key_pair, &relay_url) {
+            Ok(userId) => userId,
+            Err(e) => return Err(format!("Error signing up: {}", e)),
+        };
+
+        let homeserver_url = match &self
+            .resolver
+            .resolve_homeserver(&key_pair.public_key, &relay_url)
+        {
+            Ok(url) => url,
+            Err(e) => return Err(fromat!("Error resolving homeserver: {}", e)),
+        };
+
+        let target_url = match relay_url {
+            Some(url) => url.join(format!("/mvp/users/{}/pkarr", userId)).unwrap(),
+            None => homeserver_url
+                .join(format!("/mvp/users/{}/pkarr", userId))
+                .unwrap(),
+        };
+
+        let _ = match &self
+            .resolver
+            .publish(&key_pair, &homeserver_url, target_url)
+        {
+            Ok(_) => (),
+            Err(e) => return Err(format!("Error publishing public key: {}", e)),
+        };
+
+        crypto::zeroize(&key_pair.private_key.as_mut());
+
+        return Ok(userId)
     }
 
     /// Login to an account at the config homeserver
     pub fn login(seed: &str) -> Result<&str, Error> {
         /// TOOD:
         // create keypair from seed
-        let keypair = DeterministicKeyGen::generate(Some(seed));
+        let key_pair = DeterministicKeyGen::generate(Some(seed));
         // send user root signature as login
         // zeroize private keypair
         // return null or userId ?
@@ -66,7 +88,7 @@ impl Auth {
     fn send_user_root_signature(
         &self,
         sig_type: &SigType,
-        keypair: Keypair,
+        key_pair: Keypair,
         relay_url: Option<&Url>,
     ) -> Result<&str, String> {
         let path = match sig_type {
@@ -75,12 +97,12 @@ impl Auth {
             _ => return Err("Invalid signature type"),
         };
 
-        let challenge = self.get_challenge(keypair.public_key, None);
-        let signature = keypair.sign(&challenge.signable);
+        let challenge = self.get_challenge(key_pair.public_key, None);
+        let signature = key_pair.sign(&challenge.signable);
         if signature.as_str().len() != 64 {
             return Err("Invalid signature length");
         }
-        let userId = keypair.to_z32();
+        let userId = key_pair.to_z32();
 
         let homeserver_url = match &self.resolver.resolve_homeserver(&public_key, &relay_url) {
             Ok(url) => url,
