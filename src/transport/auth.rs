3b82963476494efa94fae9b9d00398e6e77ca238
dzdidi <deniszalessky@gmail.com>
2024-04-09 17:59:14 +0200

    auth: send_user_root_signature draft
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/transport/auth.rs
+++ src/transport/auth.rs
@@ -1,11 +1,16 @@
+mod auth;
 mod crypto;
 mod transport;
-mod auth;
 
 use auth::Challenge;
-use crypto::DeterministicKeyPair;
+use crypto::{DeterministicKeyGen, Kaypair, PublicKey};
 use transport::http::{request, Method};
 use transport::Resolver;
+
+pub enum SigType {
+    Signup,
+    Login,
+}
 
 pub struct Auth {
     resolver: Resolver,
@@ -24,7 +29,7 @@ impl Auth {
     pub fn signup(seed: &str) -> Result<&str, Error> {
         // TODO:
         // generate keypair from seed
-        let keypair = DeterministicKeyPair::generate(Some(seed));
+        let keypair = DeterministicKeyGen::generate(Some(seed));
         // send user root signature as signup
         // create signed packet with keypair and homeserverId
         // userId = encode public key into z32
@@ -38,7 +43,7 @@ impl Auth {
     pub fn login(seed: &str) -> Result<&str, Error> {
         /// TOOD:
         // create keypair from seed
-        let keypair = DeterministicKeyPair::generate(Some(seed));
+        let keypair = DeterministicKeyGen::generate(Some(seed));
         // send user root signature as login
         // zeroize private keypair
         // return null or userId ?
@@ -58,23 +63,57 @@ impl Auth {
     }
 
     /// Get challenge, sign it and authenticate
-    pub fn send_user_root_signature(sig_type: &str, keypair: &str) -> Result<&str, Error> {
-        // TODO:
-        // get challenge
-        // sign challenge
-        // encode userId to z32
-        // depending type "signup" or "login" send challenge signature to homeserver to
-        //   - mvep/users/{userId} - for signup
-        //   - mvep/session/{userId} - for Login
-        // return userId
+    fn send_user_root_signature(
+        &self,
+        sig_type: &SigType,
+        keypair: Keypair,
+        relay_url: Option<&Url>,
+    ) -> Result<&str, String> {
+        let path = match sig_type {
+            SigType::Signup => format!("/mvp/users/{}/pkarr", userId),
+            SigType::Login => format!("/mvp/session/{}", userId),
+            _ => return Err("Invalid signature type"),
+        };
+
+        let challenge = self.get_challenge(keypair.public_key, None);
+        let signature = keypair.sign(&challenge.signable);
+        if signature.as_str().len() != 64 {
+            return Err("Invalid signature length");
+        }
+        let userId = keypair.to_z32();
+
+        let homeserver_url = match &self.resolver.resolve_homeserver(&public_key, &relay_url) {
+            Ok(url) => url,
+            Err(e) => return Err(fromat!("Error resolving homeserver: {}", e)),
+        };
+
+        let url = homeserver_url.join(path).unwrap();
+
+        let mut headers = HashMap::new();
+        headers.insert("Content-Type", "application/octet-stream");
+        headers.insert("Content-Length", signature.as_str().len().to_string());
+
+        let response = request(
+            Method::PUT,
+            url,
+            &mut self.session_id,
+            Some(&headers),
+            Some(&signature),
+        );
+
+        match response {
+            Ok(_) => Ok(userId),
+            Err(e) => return Err(format!("Error sending user root signature: {}", e)),
+        }
     }
 
     /// Get challenge
-    pub fn get_challenge(
+    fn get_challenge(
+        &self,
         public_key: &PublicKey,
         relay_url: Option<&Url>,
     ) -> Result<&Challenge, Error> {
-        let homeserver_url = match self.resolver.resolve_homeserver(&public_key, &relay_url) {
+        let homeserver_url = match &self.resolver.resolve_homeserver(&public_key, &relay_url) {
             Ok(url) => url,
             Err(e) => return Err(fromat!("Error resolving homeserver: {}", e)),
         };
