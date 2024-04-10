45a36b528ddbb5e2fc9b544b84076a1f21ef04e4
dzdidi <deniszalessky@gmail.com>
2024-04-10 13:18:41 +0200

    auth: not yet tested
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/transport/auth.rs
+++ src/transport/auth.rs
@@ -1,25 +1,21 @@
-mod auth;
-mod crypto;
-mod transport;
-
-use auth::Challenge;
-use crypto::{DeterministicKeyGen, Kaypair, PublicKey};
-use transport::http::{request, Method};
-use transport::Resolver;
+use crate::transport::challenge::Challenge;
+use crate::transport::http::{request, Method, Url, HeaderMap};
+use crate::transport::resolver::Resolver;
+use crate::transport::crypto::{DeterministicKeyGen, Keypair, PublicKey, zeroize};
 
 pub enum SigType {
     Signup,
     Login,
 }
 
-pub struct Auth {
-    resolver: Resolver,
-    pub homeserver_url: &mut Option<Url>,
-    pub session_id: &mut Option<String>,
+pub struct Auth<'a> {
+    pub homeserver_url: Option<Url>,
+    pub session_id: Option<String>,
+    resolver: Resolver<'a>,
 }
 
-impl Auth {
-    pub fn new(resolver: Resolver, homeserver_url: Option<&Url>) -> Auth {
+impl Auth<'_> {
+    pub fn new(resolver: Resolver, homeserver_url: Option<Url>) -> Auth {
         Auth {
             resolver,
             session_id: None,
@@ -28,75 +24,76 @@ impl Auth {
     }
 
     /// Create a new account at the config homeserver
-    pub fn signup(seed: &str, relay_url: Option<&Url>) -> Result<&str, String> {
-        let key_pair = DeterministicKeyGen::generate(Some(seed));
-        let user_id = match self.send_user_root_signature(SigType::Signup, key_pair, &relay_url) {
+    pub fn signup(&mut self, seed: &[u8; 32], relay_url: Option<&Url>) -> Result<String, String> {
+        let key_pair = &DeterministicKeyGen::generate(Some(seed));
+        let user_id = match self.send_user_root_signature(&SigType::Signup, key_pair, relay_url) {
             Ok(user_id) => user_id,
             Err(e) => return Err(format!("Error signing up: {}", e)),
         };
 
-        if &self.homeserver_url.is_none() {
-            &self.homeserver_url = match &self
+        if self.homeserver_url.is_none() {
+            self.homeserver_url = match self
                 .resolver
-                .resolve_homeserver(&key_pair.public_key, &relay_url)
+                .resolve_homeserver(&key_pair.public_key(), relay_url)
             {
                 Ok(url) => Some(url),
-                Err(e) => return Err(fromat!("Error resolving homeserver: {}", e)),
+                Err(e) => return Err(format!("Error resolving homeserver: {}", e)),
             };
         }
 
         let target_url = match relay_url {
-            Some(url) => url.join(format!("/mvp/users/{}/pkarr", user_id)).unwrap(),
-            None => &self
-                .homeserver_url
-                .unwrap()
-                .join(format!("/mvp/users/{}/pkarr", user_id))
-                .unwrap(),
+            Some(url) => url.join(&format!("/mvp/users/{}/pkarr", user_id).as_str()).unwrap(),
+            None => {
+                let url = <Option<Url> as Clone>::clone(&self.homeserver_url).unwrap().clone();
+                let path = format!("/mvp/users/{}/pkarr", user_id);
+
+                url.join(&path.as_str()).unwrap()
+            },
         };
 
+        let url = <Option<Url> as Clone>::clone(&self.homeserver_url).unwrap().clone();
         let _ = match &self
             .resolver
-            .publish(&key_pair, &self.homeserver_url.unwrap(), target_url)
+            .publish(key_pair, &url, Some(&target_url))
         {
             Ok(_) => (),
             Err(e) => return Err(format!("Error publishing public key: {}", e)),
         };
 
-        crypto::zeroize(&key_pair.private_key.as_mut());
+        zeroize(key_pair.secret_key().as_mut());
 
-        return Ok(user_id);
+        return Ok(user_id.to_string());
     }
 
     /// Login to an account at the config homeserver
-    pub fn login(&self, seed: &str, relay_url: Option<&Url>) -> Result<&str, Error> {
-        let key_pair = DeterministicKeyGen::generate(Some(seed));
-        let user_id = match self.send_user_root_signature(SigType::Login, key_pair, &relay_url) {
+    pub fn login(&mut self, seed: &[u8; 32], relay_url: Option<&Url>) -> Result<String, String> {
+        let key_pair = &DeterministicKeyGen::generate(Some(seed));
+        let user_id = match self.send_user_root_signature(&SigType::Login, key_pair, relay_url) {
             Ok(user_id) => user_id,
             Err(e) => return Err(format!("Error signing up: {}", e)),
         };
 
-        crypto::zeroize(&key_pair.private_key.as_mut());
+        zeroize(key_pair.secret_key().as_mut());
 
         return Ok(user_id);
     }
 
     /// Logout from a specific account at the config homeserver
-    pub fn logout(user_id: &str) -> Result<(), String> {
-        if &self.session_id.is_none() {
+    pub fn logout(&mut self, user_id: &str) -> Result<(), String> {
+        if self.session_id.is_none() {
             return Err("No session found".to_string());
         }
 
-        if &self.homeserver_url.is_none() {
+        if self.homeserver_url.is_none() {
             return Err("No homeserver found".to_string());
         }
 
+        let url = <Option<Url> as Clone>::clone(&self.homeserver_url).unwrap().clone();
+        let url = url.join(&format!("/mvp/session/{}", user_id).as_str()).unwrap();
+
         match request(
             Method::DELETE,
-            &self
-                .homeserver_url
-                .unwrap()
-                .join(format!("/mvp/session/{}", user_id))
-                .unwrap(),
+            url,
             &mut self.session_id,
             None,
             None,
@@ -107,15 +104,16 @@ impl Auth {
     }
 
     /// Examine the current session at the config homeserver
-    pub fn session(&self) -> Result<&str, String> {
+    pub fn session(&mut self) -> Result<String, String> {
         // GET /mvp/session
-        if &self.homeserver_url.is_none() {
+        if self.homeserver_url.is_none() {
             return Err("No homeserver found".to_string());
         }
 
-        let url = &self.homeserver_url.unwrap().join("/mvp/session").unwrap();
+        let url = <Option<Url> as Clone>::clone(&self.homeserver_url).unwrap().clone();
+        let url = url.join("/mvp/session").unwrap();
 
-        match request(Method::GET, url, &mut self.session_id, None, None) {
+        match request(Method::GET, url.clone(), &mut self.session_id, None, None) {
             Ok(response) => {
                 // TODO: proper format of response
                 // {
@@ -126,7 +124,7 @@ impl Auth {
                 //  }
                 // } | null
                 // let session = serde_json::from_str(response).unwrap();
-                Ok(response)
+                Ok(response.to_string())
             }
             Err(e) => return Err(format!("Error getting session: {}", e)),
         }
@@ -134,72 +132,73 @@ impl Auth {
 
     /// Get challenge, sign it and authenticate
     fn send_user_root_signature(
-        &self,
+        &mut self,
         sig_type: &SigType,
-        key_pair: Keypair,
+        key_pair: &Keypair,
         relay_url: Option<&Url>,
-    ) -> Result<&str, String> {
-        let path = match sig_type {
-            SigType::Signup => format!("/mvp/users/{}/pkarr", user_id),
-            SigType::Login => format!("/mvp/session/{}", user_id),
-            _ => return Err("Invalid signature type"),
-        };
-
-        let challenge = self.get_challenge(key_pair.public_key, None);
-        let signature = key_pair.sign(&challenge.signable);
-        if signature.as_str().len() != 64 {
-            return Err("Invalid signature length");
+    ) -> Result<String, String> {
+        let challenge = self.get_challenge(&key_pair.public_key(), None);
+        let signature = key_pair.sign(&challenge.unwrap().signable).to_string();
+        if signature.len() != 64 {
+            return Err("Invalid signature length".to_string());
         }
         let user_id = key_pair.to_z32();
 
-        if &self.homeserver_url.is_none() {
-            &self.homeserver_url = match &self
+        if self.homeserver_url.is_none() {
+            self.homeserver_url = match self
                 .resolver
-                .resolve_homeserver(&key_pair.public_key, &relay_url)
+                .resolve_homeserver(&key_pair.public_key(), relay_url)
             {
                 Ok(url) => Some(url),
-                Err(e) => return Err(fromat!("Error resolving homeserver: {}", e)),
+                Err(e) => return Err(format!("Error resolving homeserver: {}", e)),
             };
         }
 
-        let url = &self.homeserver_url.unwrap().join(path).unwrap();
+        let path = match sig_type {
+            SigType::Signup => format!("/mvp/users/{}/pkarr", user_id),
+            SigType::Login => format!("/mvp/session/{}", user_id),
+        };
 
-        let mut headers = HashMap::new();
-        headers.insert("Content-Type", "application/octet-stream");
-        headers.insert("Content-Length", signature.as_str().len().to_string());
+        let url = <Option<Url> as Clone>::clone(&self.homeserver_url).unwrap().clone();
+        let url = url.join(path.as_str()).unwrap();
+
+        let mut headers = HeaderMap::new();
+        headers.insert("Content-Type", "application/octet-stream".try_into().unwrap());
+        headers.insert("Content-Length", signature.len().try_into().unwrap());
 
         let response = request(
             Method::PUT,
-            url,
+            url.clone(),
             &mut self.session_id,
             Some(&headers),
-            Some(&signature),
+            Some(signature.to_string()),
         );
 
         match response {
-            Ok(_) => Ok(user_id),
+            Ok(_) => Ok(user_id.to_string()),
             Err(e) => return Err(format!("Error sending user root signature: {}", e)),
         }
     }
 
     /// Get challenge
     fn get_challenge(
-        &self,
+        &mut self,
         public_key: &PublicKey,
         relay_url: Option<&Url>,
-    ) -> Result<&Challenge, Error> {
-        if &self.homeserver_url.is_none() {
-            &self.homeserver_url = match &self.resolver.resolve_homeserver(&public_key, &relay_url)
+    ) -> Result<Challenge, String> {
+        if self.homeserver_url.is_none() {
+            self.homeserver_url = match self.resolver.resolve_homeserver(&public_key, relay_url)
             {
                 Ok(url) => Some(url),
-                Err(e) => return Err(fromat!("Error resolving homeserver: {}", e)),
+                Err(e) => return Err(format!("Error resolving homeserver: {}", e)),
             };
         };
 
-        let url = &self.homeserver_url.unwrap().join("/mvp/challenge").unwrap();
+        let url = <Option<Url> as Clone>::clone(&self.homeserver_url).unwrap().clone();
+        let url = url.join("/mvp/challenge").unwrap();
 
-        match request(Method::GET, url, &mut self.session_id, Some(&headers), None) {
-            Ok(response) => Challenge::deserialize(response),
+        match request(Method::GET, url.clone(), &mut self.session_id, None, None) {
+            Ok(response) => Ok(Challenge::deserialize(response.as_bytes())),
             Err(e) => return Err(format!("Error getting challenge: {}", e)),
         }
     }
