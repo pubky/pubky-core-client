bc01cc7c5dceed26dfdd272929318c402155b045
dzdidi <deniszalessky@gmail.com>
2024-04-16 20:14:15 +0200

    client: get
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/client.rs
+++ src/client.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use crate::transport::{
     auth::Auth,
     crypto,
-    http::{request, Method, Url, HeaderMap},
+    http::{request, HeaderMap, Method, Url},
     resolver::Resolver,
 };
 
@@ -79,7 +79,13 @@ impl Client<'_> {
     }
 
     /// Put data into user's repository and return URL to this repo
-    pub fn put (&mut self, user_id: &str, repo_name: &str, path: &str, payload: &str) -> Result<Url, String> {
+    pub fn put(
+        &mut self,
+        user_id: &str,
+        repo_name: &str,
+        path: &str,
+        payload: &str,
+    ) -> Result<Url, String> {
         let url = &self
             .homeservers_cache
             .get(user_id)
@@ -87,14 +93,23 @@ impl Client<'_> {
             .homeserver_url
             .clone()
             .unwrap()
-            .join(&format!("/mvp/users/{}/repos/{}/{}", user_id, repo_name, path))
+            .join(&format!(
+                "/mvp/users/{}/repos/{}/{}",
+                user_id, repo_name, path
+            ))
             .unwrap();
 
         println!("Calling: {}", url);
 
         let mut headers = HeaderMap::new();
-        headers.insert("Content-Type", "application/octet-stream".try_into().unwrap());
-        headers.insert("Content-Length", payload.len().to_string().try_into().unwrap());
+        headers.insert(
+            "Content-Type",
+            "application/octet-stream".try_into().unwrap(),
+        );
+        headers.insert(
+            "Content-Length",
+            payload.len().to_string().try_into().unwrap(),
+        );
 
         let response = request(
             Method::PUT,
@@ -109,36 +124,62 @@ impl Client<'_> {
             Err(e) => return Err(e),
         }
     }
-//
-//     /// Get data from user's repository and return it as a JSON(?)
-//     pub fn get (&self, user_id: &str, repo_name: &str, path: &str) -> Result<Data, String> { }
-//
-//     /// Delete data from user's repository
-//     pub fn delete (&self, user_id: &str, repo_name: &str, path: &str) -> Result<_, String> { }
-//
-//     /// List data in user's repository
-//     /*
-//     ListOption {
-//          reverse: bool,
-//          offset: usize,
-//          limit: usize,
-//     }
-//     */
-//     pub fn list (&self, user_id: &str, repo_name: &str, path: &str, opts: Option<ListOption>) -> Result<Vec<String>, String> { }
-//
-//
-//     /// Query data in user's repository
-//     /*
-//     // Maybe can repurpose ListOption
-//     QueryOptions {
-//          reverse: bool,
-//          start: usize,
-//          end: usize,
-//          limit: usize,
-//          reverse: bool,
-//     }
-//     */
-//     pub fn query (&self, user_id: &str, repo_name: &str, query: Option<QueryOptions>) -> Result<Vec<String>, String> { }
+
+    /// Get data from user's repository and return it as a JSON(?)
+    pub fn get(&mut self, user_id: &str, repo_name: &str, path: &str) -> Result<String, String> {
+        let url = &self
+            .homeservers_cache
+            .get(user_id)
+            .unwrap()
+            .homeserver_url
+            .clone()
+            .unwrap()
+            .join(&format!(
+                "/mvp/users/{}/repos/{}/{}",
+                user_id, repo_name, path
+            ))
+            .unwrap();
+
+        let response = request(
+            Method::GET,
+            url.clone(),
+            &mut self.homeservers_cache.get_mut(user_id).unwrap().session_id,
+            None,
+            None,
+        );
+
+        match response {
+            Ok(body) => Ok(body),
+            Err(e) => return Err(e),
+        }
+    }
+    //
+    //     /// Delete data from user's repository
+    //     pub fn delete (&self, user_id: &str, repo_name: &str, path: &str) -> Result<_, String> { }
+    //
+    //     /// List data in user's repository
+    //     /*
+    //     ListOption {
+    //          reverse: bool,
+    //          offset: usize,
+    //          limit: usize,
+    //     }
+    //     */
+    //     pub fn list (&self, user_id: &str, repo_name: &str, path: &str, opts: Option<ListOption>) -> Result<Vec<String>, String> { }
+    //
+    //
+    //     /// Query data in user's repository
+    //     /*
+    //     // Maybe can repurpose ListOption
+    //     QueryOptions {
+    //          reverse: bool,
+    //          start: usize,
+    //          end: usize,
+    //          limit: usize,
+    //          reverse: bool,
+    //     }
+    //     */
+    //     pub fn query (&self, user_id: &str, repo_name: &str, query: Option<QueryOptions>) -> Result<Vec<String>, String> { }
 }
 #[cfg(test)]
 mod tests {
@@ -331,7 +372,78 @@ mod tests {
 
         let result = client.put(&user_id, repo_name, &folder_path, "test_payload");
 
-        assert_eq!(result.unwrap(), Url::parse(&server.url()).unwrap().join(&path).unwrap());
+        assert_eq!(
+            result.unwrap(),
+            Url::parse(&server.url()).unwrap().join(&path).unwrap()
+        );
+        assert_eq!(client.homeservers_cache.len(), 1);
+        assert_eq!(
+            client
+                .homeservers_cache
+                .get(&user_id)
+                .unwrap()
+                .homeserver_url,
+            Some(Url::parse(&server.url()).unwrap())
+        );
+        assert_eq!(
+            client.homeservers_cache.get(&user_id).unwrap().session_id,
+            Some("1234".to_string())
+        );
+    }
+
+    fn test_client_get() {
+        let testnet = Testnet::new(10);
+
+        let challenge = Challenge::create(now() + 1000, None);
+
+        let seed = b"it is a seed for key generation!";
+        let key_pair: Keypair = DeterministicKeyGen::generate(Some(seed));
+        let user_id = key_pair.to_z32();
+
+        let get_challange_mock_params = HttpMockParams {
+            method: &Method::GET,
+            path: "/mvp/challenge",
+            body: &challenge.serialize(),
+            status: 200,
+            headers: vec![],
+        };
+
+        let path = format!("/mvp/users/{}/pkarr", user_id);
+        let send_user_root_signature_signup_mock_params = HttpMockParams {
+            method: &Method::PUT,
+            path: path.as_str(),
+            headers: vec![("Set-Cookie", "sessionId=123")],
+            status: 200,
+            body: &b"ok".to_vec(),
+        };
+
+        let repo_name = "test_repo";
+        let folder_path = "test_path";
+        let path = format!("/mvp/users/{}/repos/{}/{}", user_id, repo_name, folder_path);
+        let create_repo_mock_params = HttpMockParams {
+            method: &Method::GET,
+            path: path.as_str(),
+            headers: vec![("Set-Cookie", "sessionId=1234")],
+            status: 200,
+            body: &b"totally ok".to_vec(),
+        };
+
+        let server = setup_datastore(vec![
+            get_challange_mock_params,
+            send_user_root_signature_signup_mock_params,
+            create_repo_mock_params,
+        ]);
+
+        let mut resolver = Resolver::new(None, Some(&testnet.bootstrap));
+        let _ = resolver
+            .publish(&key_pair, &Url::parse(&server.url()).unwrap(), None)
+            .unwrap();
+
+        let mut client = Client::new(Some(seed.clone()), None, None, Some(&testnet.bootstrap));
+
+        let result = client.get(&user_id, repo_name, &folder_path);
+
+        assert_eq!(result.unwrap(), "totally ok".to_string());
         assert_eq!(client.homeservers_cache.len(), 1);
         assert_eq!(
             client
