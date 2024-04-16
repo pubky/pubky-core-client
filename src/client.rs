e09440ea0e61da0384a11ad0a8198d7fcc78905e
dzdidi <deniszalessky@gmail.com>
2024-04-16 17:50:31 +0200

    client: create repo (session update doesnt work)
    Signed-off-by: dzdidi <deniszalessky@gmail.com>

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::transport::{crypto, auth::Auth, resolver::Resolver, http::Url};
+use crate::transport::{crypto, auth::Auth, resolver::Resolver, http::{Url, Method, request}};
 
 /// This is the pubky client class. It is used for accessing pubky infrastructure for CRUD options
 /// over user's data in pubky network.
@@ -24,27 +24,7 @@ pub struct Client {
     homeserver_url: Url,
 }
 
-impl Client {
-    pub fn new(seed: Option<[u8; 32]>, homeserver_url: Option<Url>, dht_relay: Option<Url>) -> Client {
-        let seed = seed.unwrap_or(crypto::random_bytes(32).try_into().unwrap());
-
-        let resolver = Resolver::new(dht_relay.as_ref(), None);
-        let mut auth = Auth::new(resolver, homeserver_url);
-
-        let user_id = auth.signup(&seed, None).unwrap();
-        let homeserver_url = auth.homeserver_url.unwrap();
-        let session_id = auth.session_id.unwrap();
-
-        let mut homeservers_cache = HashMap::new();
-        homeservers_cache.insert(user_id, (homeserver_url.clone(), session_id));
-
-        Client {
-            seed,
-            homeservers_cache,
-            homeserver_url,
-        }
-    }
-
+impl Client<> {
 //     pub fn new(homeserverId: &str, config: &ClientConfig) -> Client {
 //         let relay = config.relay.unwrap_or("relay.pkarr.org");
 //         let mut homeserver_url = config.homeserver_url.unwrap();
@@ -67,13 +47,41 @@ impl Client {
 //             homeservers_cache: HashMap::new(),
 //         }
 //     }
-//
-//     /* "repos" related logic */
-//     /// Create repository for user
-//     pub fn create(&self, user_id:&str, repo_name: &str) -> Result<_, String> {
-//
-//         Ok()
-//     };
+    pub fn new(seed: Option<[u8; 32]>, homeserver_url: Option<Url>, dht_relay: Option<Url>, bootstrap: Option<& Vec<String>>) -> Client {
+        let seed = seed.unwrap_or(crypto::random_bytes(32).try_into().unwrap());
+
+        let resolver = Resolver::new(dht_relay.as_ref(), bootstrap);
+        let mut auth = Auth::new(resolver, homeserver_url);
+
+        let user_id = auth.signup(&seed, None).unwrap();
+        let homeserver_url = auth.homeserver_url.unwrap();
+        let session_id = auth.session_id.unwrap();
+
+        let mut homeservers_cache = HashMap::new();
+        homeservers_cache.insert(user_id, (homeserver_url.clone(), session_id));
+
+        Client {
+            seed,
+            homeservers_cache,
+            homeserver_url,
+        }
+    }
+
+    /* "REPOS" RELATED LOGIC */
+
+    /// Create repository for user
+    pub fn create(&mut self, user_id: &str, repo_name: &str) -> Result<(), String> {
+        let url = &self.homeservers_cache.get(user_id).unwrap().0;
+        let url = url.join(&format!("/mvp/users/{}/repos/{}", user_id, repo_name)).unwrap();
+
+        let mut session_id = &self.homeservers_cache.get_mut(user_id).unwrap().1;
+        // XXX: make sure that it will update session in the cache preferably by ref
+        match request(Method::PUT, url.clone(), &mut Some(session_id.to_string()), None, None) {
+            Ok(_) => Ok(()),
+            Err(e) => return Err(e),
+        }
+    }
+
 //     // get, delete, list, query
 //     /// Put data into user's repository and return URL to this repo
 //     pub fn put (&self, user_id:&str, repo_name: &str, path: &str, payload: &str) -> Result<Url, String> {
@@ -109,4 +117,125 @@ impl Client {
 //     }
 //     */
 //     pub fn query (&self, user_id: &str, repo_name: &str, query: Option<QueryOptions>) -> Result<Vec<String>, String> { }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_utils::{setup_datastore, HttpMockParams};
+    use crate::transport::crypto::{Keypair, DeterministicKeyGen};
+    use crate::transport::challenge::Challenge;
+    use mainline::dht::Testnet;
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    // TODO: move to helper
+    fn now() -> u64 {
+        let now = SystemTime::now();
+        now.duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs()
+    }
+
+
+    #[test]
+    fn test_client_new() {
+        let testnet = Testnet::new(10);
+        let challenge = Challenge::create(now() + 1000, None);
+
+        let seed = b"it is a seed for key generation!";
+        let key_pair: Keypair = DeterministicKeyGen::generate(Some(seed));
+        let user_id = key_pair.to_z32();
+
+        let get_challange_mock_params = HttpMockParams {
+            method: &Method::GET,
+            path: "/mvp/challenge",
+            body: &challenge.serialize(),
+            status: 200,
+            headers: vec![],
+        };
+
+        let path = format!("/mvp/users/{}/pkarr", user_id);
+        let send_user_root_signature_signup_mock_params = HttpMockParams {
+            method: &Method::PUT,
+            path: path.as_str(),
+            headers: vec![("Set-Cookie", "sessionId=123")],
+            status: 200,
+            body: &b"ok".to_vec(),
+        };
+
+        let server = setup_datastore(vec![
+            get_challange_mock_params,
+            send_user_root_signature_signup_mock_params,
+        ]);
+
+
+        let mut resolver = Resolver::new(None, Some(&testnet.bootstrap));
+        let _ = resolver
+            .publish(&key_pair, &Url::parse(&server.url()).unwrap(), None)
+            .unwrap();
+
+        let client = Client::new(Some(seed.clone()), None, None, Some(&testnet.bootstrap));
+
+        assert_eq!(client.homeservers_cache.len(), 1);
+        assert_eq!(client.homeservers_cache.get(&user_id).unwrap().0, Url::parse(&server.url()).unwrap());
+        assert_eq!(client.homeservers_cache.get(&user_id).unwrap().1.clone(), "123".to_string());
+    }
+
+    #[test]
+    fn test_client_create() {
+        let testnet = Testnet::new(10);
+
+        let challenge = Challenge::create(now() + 1000, None);
+
+        let seed = b"it is a seed for key generation!";
+        let key_pair: Keypair = DeterministicKeyGen::generate(Some(seed));
+        let user_id = key_pair.to_z32();
+
+        let get_challange_mock_params = HttpMockParams {
+            method: &Method::GET,
+            path: "/mvp/challenge",
+            body: &challenge.serialize(),
+            status: 200,
+            headers: vec![],
+        };
+
+        let path = format!("/mvp/users/{}/pkarr", user_id);
+        let send_user_root_signature_signup_mock_params = HttpMockParams {
+            method: &Method::PUT,
+            path: path.as_str(),
+            headers: vec![("Set-Cookie", "sessionId=123")],
+            status: 200,
+            body: &b"ok".to_vec(),
+        };
+
+        let repo_name = "test_repo";
+        let path = format!("/mvp/users/{}/repos/{}", user_id, repo_name);
+        let create_repo_mock_params = HttpMockParams {
+            method: &Method::PUT,
+            path: path.as_str(),
+            headers: vec![("Set-Cookie", "sessionId=1234")],
+            status: 200,
+            body: &b"very ok".to_vec(),
+        };
+
+        let server = setup_datastore(vec![
+            get_challange_mock_params,
+            send_user_root_signature_signup_mock_params,
+            create_repo_mock_params,
+        ]);
+
+
+        let mut resolver = Resolver::new(None, Some(&testnet.bootstrap));
+        let _ = resolver
+            .publish(&key_pair, &Url::parse(&server.url()).unwrap(), None)
+            .unwrap();
+
+        let mut client = Client::new(Some(seed.clone()), None, None, Some(&testnet.bootstrap));
+
+        let result = client.create(&user_id, repo_name);
+
+        assert_eq!(result, Ok(()));
+        assert_eq!(client.homeservers_cache.len(), 1);
+        assert_eq!(client.homeservers_cache.get(&user_id).unwrap().0, Url::parse(&server.url()).unwrap());
+        assert_eq!(client.homeservers_cache.get(&user_id).unwrap().1.clone(), "1234".to_string());
+    }
 }
